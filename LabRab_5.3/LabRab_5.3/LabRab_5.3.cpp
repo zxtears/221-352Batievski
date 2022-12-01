15cf313636ec89fa594f9fbb6380e9b45211486b
zxtears <91019504+zxtears@users.noreply.github.com>
2022-12-01 23:17:30 +0300

    Add files via upload

---
+++ LabRab_5.3/LabRab_5.3/LabRab_5.3.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include "Matr3Diag.h"
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	Matr3Diag matrA;
+	matrA.input(10);
+	Matr3Diag matrB;
+
+	cin >> matrB;
+
+	cout << "matrA:" << endl;
+	cout << matrA << endl;
+
+	cout << "matrB:" << endl;
+	cout << matrB << endl;
+
+	cout << "matrA += matrB" << endl;
+	matrA += matrB;
+	cout << matrA << endl;
+
+	cout << "matrA -= matrB" << endl;
+	matrA -= matrB;
+	cout << matrA << endl;
+
+	cout << "matrA += matrB" << endl;
+	matrA += matrB;
+	cout << matrA << endl;
+
+	cout << "matrA + matrB" << endl;
+	cout << matrA + matrB << endl;
+	cout << "matrA - matrB" << endl;
+	cout << matrA - matrB << endl;
+	cout << "-matrA" << endl;
+	cout << (-matrA) << endl;
+	cout << "matrA * matrB" << endl;
+	cout << matrA * matrB << endl;
+	cout << "matrA * 5" << endl;
+	cout << matrA * 5 << endl;
+	cout << "matrB det()" << endl;
+	cout << matrB.det() << endl;
+	cout << "matrB trace()" << endl;
+	cout << matrB.trace() << endl;
+
+}
