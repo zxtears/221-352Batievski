8259bfb8aeda7d42d053e209a300c4b7a2a4b2fb
zxtears <91019504+zxtears@users.noreply.github.com>
2022-12-01 21:17:38 +0300

    Add files via upload

---
+++ LabRab4.1/LabRab4.1/LabRab4.1.cpp
@@ -0,0 +1,104 @@
+﻿#include <iostream>;
+#include <cstring>;
+using namespace std;
+bool checkstr(char s[100]) {
+	int len = strlen(s);
+	for (int i = 0; i < len / 2; i++) {
+		if (s[i] != s[len - i - 1]) {
+			return false;
+
+		}
+	}
+	return true;
+}
+void task1() {
+	char s[100];
+	cout << "Введите строку: ";
+	cin >> s;
+	if (checkstr(s)) {
+		cout << "Строка является полиндромом" << endl;
+	}
+	else {
+		cout << "Строка не является полиндромом" << endl;
+	}
+}
+void task2() {
+	string s, ss;
+	int n = 0, p = 0;
+	cout << "Введите строку: ";
+	cin >> s;
+	cout << "Введите подстроку: ";
+	cin >> ss;
+	while ((p = s.find(ss, p)) != s.npos) {
+		++n;
+		++p;
+	}
+	cout << "Подстрока '" << ss << "' встречается в строке '" << s << "' " << n << " раз" << endl;
+}
+void task3() {
+	int k;
+	char buff[50];
+	cout << "Введите сообщение : ";
+	cin >> buff;
+	cout << "Введите кол-во букв для сдвига : ";
+	cin >> k;
+	for (int i = 0; i < strlen(buff); i++)
+		buff[i] += k;
+	cout << "Шифр : " << buff << endl;
+
+	cout << "Расшифровка : ";
+	for (int i = 0; i < strlen(buff); i++)
+		buff[i] -= k;
+
+	cout << buff << endl;
+}
+void task4() {
+	char s[100];
+	char str = ' " ';
+	cout << "Введите строку: " << endl;
+	cin.getline(s, 100);
+	cin.ignore();
+	int len = strlen(s);
+	for (int k = 0; k < len; k++) {
+		if (s[k] == str) {
+			for (int i = k + 1; i < len; i++) {
+				if (s[i] != str) {
+					cout << s[i] << endl;
+				}
+			}
+		}
+	}
+}
+int main() {
+	setlocale(0, "");
+	int choice = 0;
+	while (true) {
+		cout << "1.Задание 1" << endl;
+		cout << "2.Задание 2" << endl;
+		cout << "3.Задание 3" << endl;
+		cout << "4.Задание 4" << endl;
+		cout << "5.Выход" << endl;
+		cin >> choice;
+		switch (choice) {
+		case 1: {
+			task1();
+			break;
+		}
+		case 2: {
+			task2();
+			break;
+		}
+		case 3: {
+			task3();
+			break;
+		}
+		case 4: {
+			task4();
+			break;
+		}
+		default: {
+			return 0;
+		}
+		}
+	}
+}
