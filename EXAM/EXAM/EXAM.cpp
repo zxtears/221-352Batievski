745755fb527e0032e73369c7a43bac0636c98d22
zxtears <91019504+zxtears@users.noreply.github.com>
2023-01-19 13:10:14 +0300

    Add files via upload

---
+++ EXAM/EXAM/EXAM.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Game_Batievski 
+{
+	//св-ва 
+private:
+	int player;
+	int gold;
+	int wood;
+	int stone;
+	int jeton;
+	int kub1;
+	int kub2;
+	int kub3;
+	int kubdop;
+	bool sovetnik;
+	int reestr;
+	int ochki;
+	int building;
+	resourses() {
+
+	}
+	vector <unsigned int> player;
+
+	//конструктор 
+public:
+
+
+
+}
+void print()
+{
+
+}
+int main()
+{
+	
+}
+
+
