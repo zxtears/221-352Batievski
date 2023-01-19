5833d5e295d19c5dac1caf38b2ac86b9a05ec780
zxtears <91019504+zxtears@users.noreply.github.com>
2023-01-19 16:57:38 +0300

    Add files via upload

---
+++ EXAM 2/EXAM 2/EXAM 2.cpp
@@ -0,0 +1,80 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+Game_Batievski::Game_Batievski() {   //пространство имен 
+
+}
+
+class Game_Batievski
+{
+	//св-ва 
+private:
+	enum Structure //список констант для построек 
+	{
+
+	};
+	struct player //структура для игрока, прописываем все что будет у игрока 
+	{
+		string playername; //имя
+		int kub1, kub2, kub3, kubdop, gold, stone, wood
+
+	};
+	struct building  //структура для построек 
+	{
+
+	};
+	vector <building> bld;
+	vector <player> play;
+
+	int year;
+	int phase;
+	string playername;
+
+
+	resourses(int) {
+
+	}
+	vector <unsigned int> player;
+
+	//конструктор 
+
+public:
+	Game_Batievski();
+
+
+
+}
+void building() {
+
+}
+//функции фаз 
+void phase1() {
+
+}
+
+void phase2() {
+
+}
+
+void phase3() {
+
+}
+
+void phase4() {
+
+}
+
+void phase5() {
+
+}
+//
+void Game_Batievski() {
+
+}
+
+
+int main()
+{
+	cout <<
+}
