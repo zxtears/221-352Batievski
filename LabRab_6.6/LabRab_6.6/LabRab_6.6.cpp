c9e78300e2f8ef8b7b90ea0e5130dcdb797de205
zxtears <91019504+zxtears@users.noreply.github.com>
2023-03-18 16:05:35 +0300

    Add files via upload

---
+++ LabRab_6.6/LabRab_6.6/LabRab_6.6.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    string word;
+    int n;
+    cout << "Введите колличество символов" << endl;
+    cin >> n;
+    map<string, int> mm;
+    while (cin >> word) // конец ввода ctrl^z потом enter
+        mm[word]++; // считаем слова
+    map<string, int>::iterator it;
+    for (it = mm.begin(); it != mm.end(); ++it)
+        cout << it->first << ':' << it->second*100/n <<"%"<< endl;
+    system("pause");
+    return 0;
+}
