00a0f20772633264739fbb44799cae495f1f9cdd
zxtears <91019504+zxtears@users.noreply.github.com>
2023-04-04 03:29:52 +0300

    Add files via upload

---
+++ Lab_Rab 6.1 stack/Lab_Rab 6.1 stack/Lab_Rab 6.1 stack.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <stack>
+using namespace std;
+
+bool balanced(const std::string& s) {
+    std::stack<char> stack;
+    for (char c : s) {
+        switch (c) {
+
+        case '(': stack.push(')'); break;
+        case '[': stack.push(']'); break;
+        case '{': stack.push('}'); break;
+        case '<': stack.push('>'); break;
+
+        case ')':
+        case ']':
+        case '}':
+        case '>':
+            if (stack.empty() || stack.top() != c) { // если стэк пуст 
+                return false;
+            }
+            stack.pop();
+            break;
+        default:
+            break;
+        }
+    }
+    return stack.empty();
+}
+
+void test(const std::string& s) {
+    
+    std::cout << '"' << s << "\" " << (balanced(s) ? "yes" : "no") << '\n';
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+	cout << "Введите выражение в скобках";
+    string s;
+    cin >> s;
+    test(s); 
+
+} 
+
+
+    
