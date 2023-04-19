0191f4b00ad2800b5d9132e4f6c96ed3a5d2cace
zxtears <91019504+zxtears@users.noreply.github.com>
2023-04-20 02:17:56 +0300

    Update LabRab_6.3.cpp

--- LabRab_6.3/LabRab_6.3/LabRab_6.3.cpp
+++ LabRab_6.3/LabRab_6.3/LabRab_6.3.cpp
@@ -1,30 +1,46 @@
-﻿#include <iostream>
+#include <iostream> //FIFO (First-In, First-Out).
+#include <queue>
 
 using namespace std;
 
-unsigned Div(unsigned a) {
-    while (a % 2 == 0)
-        a /= 2;
-    while (a % 3 == 0)
-        a /= 3;
-    while (a % 5 == 0)
-        a /= 5;
-    return a;
+bool proverka(int n){
+    bool mn = true;
+    int i = 2;
+    while (i * i <= n) {
+        while (n % i == 0) {
+            if (i != 2 and i != 3 and i != 5)
+                mn = false;
+            n /= i;
+        }
+        i++;
+    }
+    if (n != 2 and n != 3 and n != 5)
+        mn = false;
+    return mn;
+}
+
+void task() {
+    queue <int> j;
+    int n;
+    cout << "VVedite chislo";
+    cin >> n;
+    int i = 0;
+    int k = 0;
+    while (i != n) {
+        if (proverka(k)) {
+            j.push(k);
+            i++;
+        }
+        k++;
+    }
+    int m = j.size();
+    for (int i = 0; i < m; i++) {
+        cout << j.front() <<'\n';
+        j.pop();
+    }
+
 }
 
 int main() {
-
-    ios_base::sync_with_stdio(0);
-    cin.tie(NULL);
-
-    unsigned a = 2, count = 0, n;
-    cin >> n;
-    while (count < n) {
-        if (Div(a) == 1) {
-            cout << a << " ";
-            ++count;
-        }
-        ++a;
-    }
-    return 0;
+    task();
 }
